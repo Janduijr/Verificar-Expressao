4ec4c470d0bbc1622b59ab4931e27135edd5eedf
JANDUI SERAFIM DA SILVA JÚNIOR <20251094040027@ifrn.local>
2026-08-26 17:46:45 -0300

    versao 1.1

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-a = '(a + b), {[a * (b + c)]}, a + {b - [c * d]}'
+a = 'a + (b, {[a * b], (a + b)}, a + {b - [c * d}'
 list = []
+abre = 0
+fecha = 0
 
 for x in a:
-    if x in ['{','}','(',')','[',']']:
+    if x in ['}',')',']']:
+        fecha += 1
+        list.insert(0, x)
+
+for x in a:
+    if x in ['{','(','[']:
+        abre += 1
         list.insert(0, x)
 
 print(list)
+if abre == fecha:
+    print('TUDO OK!')
+else:
+    print('NAO ESTA BALANCEADO!')
