9dba602b01350b82169d9754c467efa22212af96
Jandui Junior <134757940+Janduijr@users.noreply.github.com>
2026-08-30 13:38:52 -0300

    versao 2.1

--- main.py
+++ main.py
@@ -1,48 +1,56 @@
-a = ' (a + b), {[a * (b + c)]}, a + {b - [c * d]}'
-list = []
-invalido = False
-
-for x in a:
-    if x in ['{','[','(']:
-        list.append(x)
+def verifica(x):
+    global invalido
     if x == ')':
         if len(list) > 0:
             if '(' == list[-1]:
                 del list[-1]
             else:
                 invalido = True  
-                break
+                return
         else:
             invalido = True
-            break
+            return
     elif x == ']':
         if len(list) > 0:
             if '[' == list[-1]:
                 del list[-1]
             else:
                 invalido = True 
-                break
+                return
         else:
             invalido = True
-            break
+            return
     elif x == '}':
         if len(list) > 0:
             if '{' == list[-1]:
                 del list[-1]
             else:
                 invalido = True 
-                break
+                return
         else:
             invalido = True
-            break
+            return
+    
+a = '(a + b), {[a * (b + c)]}, a + {b - [c * d]}'
+list = []
+invalido = False
+
+for x in a:
+    if x in ['{','[','(']:
+        list.append(x)
+    
+    verifica(x)
+    
 
 if invalido is False:
-    if len(list) != 0:
+    if len(list) != 0:    
         print('NAO ESTA BALANCEADO!')
+        print(f'FALTOU FECHAR OS SEGUINTES TERMOS: {list}')
     else:
         print('ESTA BALANCEADO!')
 else:
     print('NAO ESTA BALANCEADO!')
+    print('VOCE FECHOU ALGO QUE NAO ESTAVA ABERTO!')
 
 
 
